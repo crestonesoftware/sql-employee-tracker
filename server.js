a329e88d8d45fa172ed170ebbcc695ffc4e4e5db
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-02 15:00:16 -0700

    call out to CLI.promptUser(), eliminates server code

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+// const express = require("express");
+//const PORT = process.env.PORT || 3019;
+// const app = express();
+
 const CLI = require("./lib/interactions.js");
 
 function init() {
   CLI.promptUser();
 }
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+//   CLI.promptUser();
+// });
 
 init();
