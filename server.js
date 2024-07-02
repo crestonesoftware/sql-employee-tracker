3070841483b8bc5e6b3b538c065a5fac39161b95
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-02 06:33:39 -0700

    moves query code to interactions.js

--- server.js
+++ server.js
@@ -1,20 +1,4 @@
-const pg = require("pg");
-const qq = require("./db/queries.js");
 const CLI = require("./lib/interactions.js");
-
-const { Pool } = pg;
-const pool = new Pool({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "postgres",
-});
-
-async function simpleQuery() {
-  let result = await pool.query(qq.ALL_EMPLOYEES);
-  console.log(result.rows);
-}
 
 function init() {
   CLI.promptUser();
